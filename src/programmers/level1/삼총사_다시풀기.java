9e12202cbddb6625c94053e0c1bb5fe125dfbb19
wisehero <godjw96@gmail.com>
2025-07-12 10:55:31 +0900

    프로그래머스 LEVEL 1 문제풀이

---
+++ src/programmers/level1/삼총사_다시풀기.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+public class 삼총사_다시풀기 {
+	public int solution(int[] number) {
+		int answer = 0;
+
+		for (int i = 0; i < number.length - 2; i++) {
+			for (int j = i + 1; j < number.length - 1; j++) {
+				for (int k = j + 1; k < number.length; k++) {
+					if (number[i] + number[j] + number[k] == 0) {
+						answer++;
+					}
+				}
+			}
+		}
+		return answer;
+	}
+}
